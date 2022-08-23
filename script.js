9ef4bf33bd220bc7c55d450af2d0fd9be93e4ad6
Yakushev Roman <boxboxbox@inbox.ru>
2022-08-23 22:16:54 +0300

    Add btn

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function FindFiles(){
-    const
-}
+let fotoQty = 6;
+let containerFoto = document.getElementById("chooseFoto");
 
-function CreateNavBarBtn(){
-
+for (let i = 0; i < fotoQty; i++) {
+    let element = document.createElement("input");
+    element.setAttribute("type", "radio");
+    element.setAttribute("id", i);
+    containerFoto.appendChild(element);
+    console.log(element);
 }
