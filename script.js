3a1a007b10a774face3e76a91ae6cc5e4cc5596a
Yakushev Roman <boxboxbox@inbox.ru>
2022-08-24 13:09:42 +0300

    All is done

--- script.js
+++ script.js
@@ -1,10 +1,87 @@
 let fotoQty = 6;
 let containerFoto = document.getElementById("chooseFoto");
+let navBar = document.getElementById("navBar");
+let foto = document.getElementById("pic");
+let radioBtn = document.getElementsByTagName('input');
+let chooseFoto = document.getElementById('chooseFoto');
+let countFoto = 1;
+let play = false;
+let playTimer;
 
-for (let i = 0; i < fotoQty; i++) {
-    let element = document.createElement("input");
-    element.setAttribute("type", "radio");
-    element.setAttribute("id", i);
-    containerFoto.appendChild(element);
-    console.log(element);
+createRadioBtn();
+changeRadioBtn();
+
+chooseFoto.addEventListener('click', (event)=> {
+    countFoto = event.target.id;
+    console.log(event.target.id);
+    changeRadioBtn();
+    changeFoto();
+});
+
+navBar.addEventListener("click", (event)=> {
+    switch(event.target.id) {
+        case 'toStart':
+            countFoto = 1;
+            changeFoto();
+            break;
+        case 'back':
+            if (countFoto > 1) {
+                countFoto--;
+                changeFoto();
+            }
+            break;
+        case 'play':
+            if (play === false) {
+                play = true;
+                playTimer = setInterval(playGallery, 3000);
+            } else if (play === true){
+                play = false;
+                clearInterval(playTimer);
+                console.log(1);
+            }
+            break;
+        case 'forward':
+            if (countFoto < fotoQty) {
+                countFoto++;
+                changeFoto();
+            }
+            break;
+        case 'toEnd':
+            countFoto = fotoQty;
+            changeFoto();
+            break;
+        default:
+            break;
+    }
+});
+
+function playGallery() {
+    if (countFoto >= 1 && countFoto <= fotoQty) {
+        changeFoto();
+        countFoto++;
+    }
+    else {
+        countFoto = 1;
+    }
+};
+
+function changeFoto(){
+    foto.src = "./files/" + countFoto + ".jpg";
+    changeRadioBtn();
+};
+
+function createRadioBtn(){
+    for (let i = 0; i < fotoQty; i++) {
+        let element = document.createElement("input");
+        element.setAttribute("type", "radio");
+        element.setAttribute("id", i+1);
+        containerFoto.appendChild(element);
+    }
+};
+
+function changeRadioBtn() {
+    for (let i = 0; i < fotoQty; i++) {
+        radioBtn[i].checked = '';
+    }
+    radioBtn[countFoto-1].checked = 'true';
 }
